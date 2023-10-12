09aa1f4b60e96eedf4c4a36ddcc042337db42db3
PujanBaniya <139758537+PujanBaniya@users.noreply.github.com>
2023-10-12 18:33:59 +0545

    Add files via upload

---
+++ random.java
@@ -0,0 +1,61 @@
+import java.util.Random;
+import java.util.Scanner;
+public class random {
+    public static void main(String[] args)
+    {
+        int a,b;
+        Random rand= new Random();
+        Scanner sc= new Scanner(System.in);
+        a= rand.nextInt(3);
+        System.out.println("press\n 1:paper\n2:scissor\n 0:rock");
+        b= sc.nextInt();
+        if(a==0)
+        {
+            System.out.println("Computer choice is Rock");
+            if(b==2)
+            {
+                System.out.println("\nComputer wins");
+            }
+            if(b==0)
+            {
+                System.out.println("\nDraw");
+            }
+            else{
+                System.out.println("\nYou win");
+            }
+        }
+        
+    else if(a==1)
+        {
+            System.out.println("Computer choice is Papper");
+            if(b==0)
+            {
+                System.out.println("\nComputer wins");
+            }
+            if(b==1)
+            {
+                System.out.println("\nDraw");
+            }
+            else{
+                System.out.println("\nYou win");
+            }
+        }
+    else
+        {
+            System.out.println("Computer choice is Scissor");
+            if(b==1)
+            {
+                System.out.println("\nComputer wins");
+            }
+            if(b==2)
+            {
+                System.out.println("\nDraw");
+            }
+            else{
+                System.out.println("\nYou win");
+            }
+        }
+        
+    }
+        
+}
